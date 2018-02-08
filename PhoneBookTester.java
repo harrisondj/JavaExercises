ce4c15ee513e1b8824fc56091d16224b8bb11708
Derek Harrison <hderek18@comcast.net>
2018-02-08 12:32:46 -0700

    Complete Exercise 1 for Phone Book

--- PhoneBookTester.java
+++ PhoneBookTester.java
@@ -7,18 +7,27 @@ public class PhoneBookTester
     PhoneBook pb = new PhoneBook();
     Scanner scanner = new Scanner(System.in);  
     // search for "Violet Smith"
-    System.out.println("Name?");
-    String first_name = scanner.next();
-    String last_name = scanner.next();
-    first_name.toUpperCase();
-    last_name.toUpperCase();
-    System.out.println(first_name + last_name);
-    PhoneEntry entry = pb.search(first_name + " " + last_name); 
+    boolean quit = false;
+    while (quit != true) {
+      System.out.println("Name?");
+      String first_name = scanner.nextLine();
+      if (first_name.equals("quit")) {
+        System.out.println("good-by");
+        quit = true;
+      } else {
+        //System.out.println("Last_name");
+        //String last_name = scanner.nextLine();
+        //String name = first_name + " " + last_name;
+        PhoneEntry entry = pb.search(first_name); 
 
-    if ( entry != null )
-      System.out.println( entry.getName() + ": " + entry.getPhone() );
-    else
-      System.out.println("Name not found" );
+        if ( entry != null )
+          System.out.println( entry.getName() + ": " + entry.getPhone() );
+        else
+          System.out.println("Name not found" );
+        
+      }
+      
+    }
 
   }
 }
